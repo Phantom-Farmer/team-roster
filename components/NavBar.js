3da032a388003fd78531b2ceeefa932068df438a
Phantom-Farmer <joeljdahl@gmail.com>
2022-07-23 12:25:46 -0500

    time for more coding

--- components/NavBar.js
+++ components/NavBar.js
@@ -11,7 +11,7 @@ export default function NavBar() {
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
         <Link passHref href="/">
-          <Navbar.Brand>CHANGE ME</Navbar.Brand>
+          <Navbar.Brand>Joelton Joltz</Navbar.Brand>
         </Link>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
@@ -19,6 +19,15 @@ export default function NavBar() {
             {/* CLOSE NAVBAR ON LINK SELECTION: https://stackoverflow.com/questions/72813635/collapse-on-select-react-bootstrap-navbar-with-nextjs-not-working */}
             <Link passHref href="/">
               <Nav.Link>Home</Nav.Link>
+            </Link>
+            <Link passHref href="/roster">
+              <Nav.Link>Team Roster</Nav.Link>
+            </Link>
+            <Link passHref href="/new">
+              <Nav.Link>Add Player</Nav.Link>
+            </Link>
+            <Link passHref href="/competition">
+              <Nav.Link>Enemy Teams</Nav.Link>
             </Link>
             <Button variant="danger" onClick={signOut}>Sign Out</Button>
           </Nav>
---
+++ pages/player/newPlayer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function NewPlayerForm() {
+  return (
+    <div>
+      <h1>Add New Player</h1>
+    </div>
+  );
+}
