ce1a1bd6edc75e694759c2752509b44c4c115a1b
Phantom-Farmer <joeljdahl@gmail.com>
2022-07-29 23:12:09 -0500

    friday night right

---
+++ components/User.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function User({
+  image, name, email, lastLogin,
+}) {
+  return (
+    <div>
+      <img src={image} alt={name} />
+      <h1>{name}</h1>
+      <h2>{email}</h2>
+      <h3>{lastLogin}</h3>,
+    </div>
+  );
+}
+
+User.propTypes = {
+  image: PropTypes.string,
+  name: PropTypes.string,
+  email: PropTypes.string,
+  lastLogin: PropTypes.string,
+};
+User.defaultProps = {
+  image: 'https://images.app.goo.gl/SbRWLNtsL2jvX5D89',
+  name: 'Joel Mcanulty',
+  email: 'joeljdahl@gmail.com',
+  lastLogin: '2022-14-07',
+};
+
+export default User;
---
+++ pages/profile.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useAuth } from '../utils/context/authContext';
+import { signOut } from '../utils/auth';
+
+export default function Profile() {
+  const { user } = useAuth();
+  return (
+    <div> <User
+      name={user.displayName}
+      email={user.email}
+      image={user.photoURL}
+      lastLogin={user.metadata.lastSignInTime}
+    />
+      <button type="button" onClick={signOut}>Sign Out</button>
+    </div>
+  );
+}
